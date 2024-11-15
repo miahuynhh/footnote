b4e666e318c0be18f29df437a469bab338635a7b
Alicia Z <aliciaw0@cs.washington.edu>
2024-11-14 20:47:58 -0800

    cleaned up

--- footnote-frontend/src/routes/Routes.tsx
+++ footnote-frontend/src/routes/Routes.tsx
@@ -1,21 +1,16 @@
 import Login from "../pages/Login/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import UserHome from "../pages/UserHome/UserHome";
 import SignUp from "../pages/SignUp/SignUp";
-import ProjectPage from "../pages/ProjectPage/ProjectPageAlicia";
-// import CreateNewProject from "../pages/CreateNewProject";
-// import HorizontalExample from "../pages/toy";
-// import CreateNewProject from "../pages/CreateNewProject";
-// import HorizontalExample from "../pages/toy";
+import AnnotationPage from "../pages/AnnotationPage";
+import UserHome from "../pages/UserHome/UserHome";
+import ProjectPage from "../pages/ProjectPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/signup", element: <SignUp /> },
   { path: "/home", element: <UserHome /> },
-  { path: "/project", element: <ProjectPage /> },
+  { path: "/project", element: <AnnotationPage /> },
   { path: "/project/:pid", element: <ProjectPage /> },
-  // { path: "/create-new", element: <CreateNewProject /> },
-  // { path: "/toy", element: <HorizontalExample /> },
 ]);
 
 export function Routes() {
